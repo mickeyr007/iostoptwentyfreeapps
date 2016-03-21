8df08b08cfec8a7be271d49b5823fe23e9fdb8ea
Mickey Raj <mickey@favoriteeats.com>
2016-03-20 21:04:51 -0600

    implement appTitleAndSubtitle for title and subtitle of the app

--- topiosapps/topiosapps/TableViewController.m
+++ topiosapps/topiosapps/TableViewController.m
@@ -7,21 +7,63 @@
 //
 
 #import "TableViewController.h"
+#import "CustomTableCell.h"
+#import "App.h"
 
 @interface TableViewController ()
+{
 
+}
 @end
 
 @implementation TableViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+     [self appTitleAndSubtitle];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)appTitleAndSubtitle
+{
+    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 0, 0)];
+    titleLabel.backgroundColor = [UIColor clearColor];
+    titleLabel.textColor = [UIColor blackColor];
+    titleLabel.font = [UIFont boldSystemFontOfSize:20];
+    titleLabel.text = @"Top 25 Free Apps";
+    [titleLabel sizeToFit];
+ 
+    UILabel *subTitleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 22, 0, 0)];
+    subTitleLabel.backgroundColor = [UIColor clearColor];
+    subTitleLabel.textColor = [UIColor blackColor];
+    subTitleLabel.font = [UIFont systemFontOfSize:12];
+    subTitleLabel.text=@"updated date";
+   
+    [subTitleLabel sizeToFit];
+    
+    UIView *twoLineTitleView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, MAX(subTitleLabel.frame.size.width, titleLabel.frame.size.width), 30)];
+    [twoLineTitleView addSubview:titleLabel];
+    [twoLineTitleView addSubview:subTitleLabel];
+    
+    float widthDiff = subTitleLabel.frame.size.width - titleLabel.frame.size.width;
+    
+    if (widthDiff > 0) {
+        CGRect frame = titleLabel.frame;
+        frame.origin.x = widthDiff / 2;
+        titleLabel.frame = CGRectIntegral(frame);
+    }else{
+        CGRect frame = subTitleLabel.frame;
+        frame.origin.x = abs(widthDiff) / 2;
+        subTitleLabel.frame = CGRectIntegral(frame);
+    }
+    
+    self.navigationItem.titleView = twoLineTitleView;
 }
 
 @end
