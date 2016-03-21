830942ea8a30a9a159390142e9f1d9063bd0c0c6
Mickey Raj <mickey@favoriteeats.com>
2016-03-20 23:26:55 -0600

    copy contents from AppTableViewController to PodcastTableViewController add Table view controller for AppTableViewController connect segue to AppTableViewController

---
+++ topiosapps/topiosapps/PodcastTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PodcastTableViewController.h
+//  topiosapps
+//
+//  Created by Mickey on 3/20/16.
+//  Copyright © 2016 Mickey Raj. All rights reserved.
+//
+
+#ifndef PodcastTableViewController_h
+#define PodcastTableViewController_h
+
+
+#endif /* PodcastTableViewController_h */
---
+++ topiosapps/topiosapps/PodcastTableViewController.m
@@ -0,0 +1,9 @@
+//
+//  PodcastTableViewController.m
+//  topiosapps
+//
+//  Created by Mickey on 3/20/16.
+//  Copyright © 2016 Mickey Raj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
